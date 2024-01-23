b09ff65ef3df85089f5644488ecb40d15f8ff73c
JOHON-DEV <machoman4701@gmail.com>
2024-01-23 06:12:51 +0500

    js-10

---
+++ index.js
@@ -0,0 +1,96 @@
+// function arrayNumber(n) {
+//   let arr = [];
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 5 == 0 && i % 3 == 0) {
+//       arr.push(i);
+//     }
+//   }
+//   return arr;
+
+// }
+// let n=100
+//  let result=arrayNumber(n)
+// console.log(result)
+
+// 4misol
+// let arr=[]
+
+// function sumToq(n){
+//     let juft = 0;
+// let toq = 0;
+// for(let i=0 ;i<n;i++){
+//     arr[i]=i*10
+//     if(i%2==0){
+//         juft-=arr[i]
+//     }else
+//     toq+=arr[i]
+// }
+// console.log(arr)
+// console.log(juft,toq)
+// }
+// sumToq(20)
+
+// 5misol
+
+// let a=prompt()
+// function ndex(n){
+//     let arr =[]
+//     let b=0
+//     for(i=0;i<n;i++){
+//         arr[i]=+prompt("son")
+//         b+=arr[i]
+//     }
+//     console.log(arr)
+//     console.log(b)
+// }
+// let n=5
+// ndex(n)
+
+// 10-misol
+// let n = 7;
+// let arr = [];
+// for(let i = 0; i<=7; i++){
+//     arr.push(Math.trunc(Math.random()*100));
+// }
+// let min = arr[0];
+// for(let i = 0; i<arr.length; i++){
+//     if(i%2==0)
+//         if(min > arr[i])
+//             min = arr[i]; 
+// }
+// console.log(arr);
+// console.log(min);
+
+// 11-misol
+// let n = 7;
+// let arr = [];
+// for(let i = 0; i<=7; i++){
+//     arr.push(Math.trunc(Math.random()*100));
+// }
+// let max = 0;
+// for(let i = 0; i<arr.length; i++){
+//     if(i%2==1)
+//         if(max < arr[i])
+//             max = arr[i]; 
+// }
+// console.log(arr);
+// console.log(max);
+
+// 12-misol
+// let n = 7;
+// let arr = [];
+// let arif = 0;
+// for(let i = 0; i<=7; i++){
+//     arr.push(Math.trunc(Math.random()*100));
+//     arif+= arr[i];
+// }
+// console.log(arif / arr.length);
+
+// 13-misol
+// let n = 7;
+// let arr = [];
+// for(let i = 0; i<=7; i++){
+//     arr.push(Math.trunc(Math.random()*100));
+//     if(i % 2 == 1)
+//          console.log(arr[i]);
+// }
---
+++ readme.md
@@ -0,0 +1,47 @@
+
+# I I F E
+
+```
+
+(function(arg){
+    //code execuit
+    console.log(arg)
+})(qiymat)
+
+
+o'zini o'zi chaqiruvchi function
+
+```
+
+-----
+# Array
+``Array literial -> ["", "12", true, 22 ]``
+
+
+```
+array.from -> jadvalga o'giradi 
+misol: array.from(" J S ")
+
+copyWithin -> ketma ket almashtiradi
+misol: let arr=[1,2,3,4,5] 
+arr.copyWithin(3,1)
+[1,2,3,2,3]
+
+pop() -> oxirgi elementdan boshlab kesadi
+
+push() -> davomidan qo'shib ketadi
+
+reverse() -> teskari tartibda joylashtiradi
+
+shift -> birinchi elementdan o'chiradi
+
+unshift -> boshidan element qo'shadi
+
+splice -> 3ta qiymat oladi splice(boshi,...dan boshlab kesadi,berilgan bolsa qiymat qoshadi)
+
+sort -> osib borish tartibi
+
+concat -> qo'shadi va yangi jadval hosil qiladi
+
+sort -> arrayni stringga ogiradi
+```
